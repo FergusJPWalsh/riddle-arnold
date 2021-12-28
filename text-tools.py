6ec9343e1d82ab30933d7b3646d8a5a752b664ff
Fergus <fergusjpwalsh@gmail.com>
2021-12-28 00:37:50 +0000

    Add in tabs
    Still many more to be added in.

--- text-tools.py
+++ text-tools.py
@@ -1,11 +1,10 @@
-# import csv
-bad_entries = 0
+import re
 with open ("riddle-arnold.tsv", "r", encoding="utf-8") as f:
     # proper_names = csv.reader(f, delimiter="\t")
     proper_names = f.readlines()
     for i, entry in enumerate(proper_names):
         tabs = entry.count("\t")
-        if tabs != 1:
-            bad_entries = bad_entries + 1
-            print(i+1,entry)
-            print(bad_entries)
+        if tabs == 0:
+            print(i+1)
+        elif tabs > 1:
+            print(i+1)
