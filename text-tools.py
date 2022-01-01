dff8ee2826d4ff637fd16c57ccd5bc54800e5e17
Fergus <fergusjpwalsh@gmail.com>
2022-01-01 18:10:32 +0000

    Remove all tab errors

--- text-tools.py
+++ text-tools.py
@@ -1,4 +1,4 @@
-import re
+bad_entries = []
 with open ("riddle-arnold.tsv", "r", encoding="utf-8") as f:
     # proper_names = csv.reader(f, delimiter="\t")
     proper_names = f.readlines()
@@ -6,5 +6,8 @@ with open ("riddle-arnold.tsv", "r", encoding="utf-8") as f:
         tabs = entry.count("\t")
         if tabs == 0:
             print(i+1)
+            bad_entries.append(entry)
         elif tabs > 1:
             print(i+1)
+            bad_entries.append(entry)
+print(len(bad_entries))
