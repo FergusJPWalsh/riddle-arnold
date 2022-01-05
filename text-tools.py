b124aa4713bdbea862811d168aa4a14a8752011d
Fergus <fergusjpwalsh@gmail.com>
2022-01-05 17:46:51 +0000

    Standardise headwords
    - Alternate headwords standardised. "GO OUT or FORTH" -> "GO OUT, GO FORTH" "COD or CODFISH" -> "COD, CODFISH"
    - En and em dashes, horiztonal bar etc. changed to hyphen.
    - Other edits.

--- text-tools.py
+++ text-tools.py
@@ -1,17 +1,29 @@
-bad_entries = []
+import csv
+import re
+headwords = []
+entry_texts = []
 with open ("riddle-arnold.tsv", "r", encoding="utf-8") as f:
-    # proper_names = csv.reader(f, delimiter="\t")
-    proper_names = f.readlines()
-    for i, entry in enumerate(proper_names):
-        tabs = entry.count("\t")
-        if tabs == 0:
-            print(i+1)
-            bad_entries.append(entry)
-        elif tabs > 1:
-            print(i+1)
-            bad_entries.append(entry)
-print(len(bad_entries))
+    lexicon = csv.reader(f, delimiter="\t")
+    for entry in lexicon:
+        headword = entry[0]
+        entry_text = entry[1]
+        headwords.append(headword)
+        entry_texts.append(entry_text)
+for i, headword in enumerate(headwords):
+    if re.search("[^A-Z]", headword):
+        print(i+1, headword)
 
-all_text = " ".join(proper_names)
-letter_set = set(all_text)
-print(letter_set)
+    # proper_names = f.readlines()
+    # for i, entry in enumerate(proper_names):
+        # tabs = entry.count("\t")
+        # if tabs == 0:
+            # print(i+1)
+            # bad_entries.append(entry)
+        # elif tabs > 1:
+            # print(i+1)
+            # bad_entries.append(entry)
+# print(len(bad_entries))
+
+# all_text = " ".join(proper_names)
+# letter_set = set(all_text)
+# print(letter_set)
