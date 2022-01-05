31dd5e5fa09a842096eec6a8fa68f9beeec22381
Fergus <fergusjpwalsh@gmail.com>
2022-01-05 21:58:05 +0000

    Character set cleaning

--- text-tools.py
+++ text-tools.py
@@ -2,28 +2,30 @@ import csv
 import re
 headwords = []
 entry_texts = []
+bad_entries = []
 with open ("riddle-arnold.tsv", "r", encoding="utf-8") as f:
-    lexicon = csv.reader(f, delimiter="\t")
-    for entry in lexicon:
-        headword = entry[0]
-        entry_text = entry[1]
-        headwords.append(headword)
-        entry_texts.append(entry_text)
-for i, headword in enumerate(headwords):
-    if re.search("[^A-Z]", headword):
-        print(i+1, headword)
+    # lexicon = csv.reader(f, delimiter="\t")
+    # for entry in lexicon:
+        # headword = entry[0]
+        # entry_text = entry[1]
+        # headwords.append(headword)
+        # entry_texts.append(entry_text)
+# for i, headword in enumerate(headwords):
+    # if re.search("[^A-Z]", headword):
+        # print(i+1, headword)
 
-    # proper_names = f.readlines()
-    # for i, entry in enumerate(proper_names):
-        # tabs = entry.count("\t")
-        # if tabs == 0:
-            # print(i+1)
-            # bad_entries.append(entry)
-        # elif tabs > 1:
-            # print(i+1)
-            # bad_entries.append(entry)
-# print(len(bad_entries))
+    lexicon = f.readlines()
+    for i, entry in enumerate(lexicon):
+        tabs = entry.count("\t")
+        if tabs == 0:
+            print(i+1)
+            bad_entries.append(entry)
+        elif tabs > 1:
+            print(i+1)
+            bad_entries.append(entry)
+print("Len bad_entries:",len(bad_entries))
+print("bad_entries:\n",bad_entries)
 
-# all_text = " ".join(proper_names)
-# letter_set = set(all_text)
-# print(letter_set)
+all_text = " ".join(lexicon)
+letter_set = set(all_text)
+print(sorted(letter_set))
